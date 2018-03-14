ee65e4ef3922cb9b7720d6fa49fc43eed50317eb
Rafael Dreher <dreher@me.com>
2018-03-13 17:44:56 -0700

    Initial

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Rafael Dreher on 7/20/17.
+//  Copyright © 2017 Rafael Dreher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Set the output of methods
+    // Drag from ViewControler to UILabel
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    // Array of constants
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Texas?",
+        "What is cognac made from?"
+    ]
+    //Array of constants
+    let answers: [String] = [
+        "14",
+        "Austin",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    // Set the actions for the methods
+    // Drag from the UIButton to the ViewControler
+    @IBAction func showNextQuestion(_sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+
+    @IBAction func showAnswer(_sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
